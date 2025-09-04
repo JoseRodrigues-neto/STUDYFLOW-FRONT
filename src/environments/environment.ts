e5774d5f80e85c27068c59c2d953915b0fcae048
Jose neto <joserodrigues15ne@gmail.com>
2025-09-04 00:25:21 -0300

    atualizacão no firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,11 @@
-export const environment = {
+ export const environment = {
   production: false,
   firebaseConfig: {
-  apiKey: "AIzaSyBTUKvyGMWpn4WZttetbSeJy9qN_rs5Q4k",
-  authDomain: "fire-flow-6aa0c.firebaseapp.com",
-  projectId: "fire-flow-6aa0c",
-  storageBucket: "fire-flow-6aa0c.firebasestorage.app",
-  messagingSenderId: "775170297156",
-  appId: "1:775170297156:web:eddcc9ec6f68ebd47c1b11",
-  measurementId: "G-WYVXD5M8D6"
+    apiKey: "AIzaSyDrN7f1IJ8ctKG3aYUfovWelIhhFrtoWzM",
+    authDomain: "studyflow-pi.firebaseapp.com",
+    projectId: "studyflow-pi",
+    storageBucket: "studyflow-pi.firebasestorage.app",
+    messagingSenderId: "262951542633",
+    appId: "1:262951542633:web:61c722951397bad57627f8"
   }
 };
